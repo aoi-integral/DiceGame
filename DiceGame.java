94f67219752567e40d76373195f72b5bf7754f9a
aoi-integral <s1280064@u-aizu.ac.jp>
2021-07-06 18:26:24 +0900

    Greetings added

--- DiceGame.java
+++ DiceGame.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
 public class DiceGame{
     public static void main(String[] args){
 
+	Scanner scanner = new Scanner(System.in);
+	System.out.println("What is your name? ");
+	String name =scanner.next();
+	System.out.println("Hello,"+name+ "!");
+	
 	int SIDES =6;
 	System.out.println("Rolling the dice...");
 	int a = 1+ (int)(Math.random() * SIDES);
